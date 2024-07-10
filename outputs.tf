75c326566a353610c57edbb261a7449696060a9e
Ghitan Iustin <iustinboss212@gmail.com>
2024-07-11 02:08:50 +0300

    Bug fix for public and private ip adress number

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,10 @@ output "vm_password" {
 
 output "vm_public_ips" {
   description = "Adresele IP publice ale masinilor virtuale"
-  value       = azurerm_public_ip.pip[*].ip_address
+   value = azurerm_public_ip.pip[*].ip_address
 } 
 
 output "vm_private_ips" {
   description = "Adresele ip private ale masinilor virtuale"
-  value = azurerm_network_interface.example[*].private_ip_address
+  value = azurerm_network_interface.example[*].ip_configuration[0].private_ip_address
 } 
