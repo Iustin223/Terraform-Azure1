34c0a12f6b3cdbae4c23f671286583d0b1ac9be8
Ghitan Iustin <iustinboss212@gmail.com>
2024-07-15 17:03:53 +0300

    Another small commit

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "vm_count" {
   description = "Number of virtual machines to create"
   type        = number
-  default     = 1
+   
 }
 
 variable "vm_size" {
   description = "Size of the virtual machines"
   type        = string
-  default     = "Standard_B1s"
+  
 }
 
 variable "vm_image" {
   description = "Image SKU for the virtual machines"
   type        = string
-  default     = "18.04-LTS"
+   
 }
--- vnet.tf
+++ vnet.tf
@@ -3,5 +3,4 @@ resource "azurerm_virtual_network" "vn" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
-  
-}
+ }
