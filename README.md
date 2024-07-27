49267d2b151511c032d635d269af1550b46c1136
Ghitan Iustin <iustinboss212@gmail.com>
2024-07-27 20:05:21 +0300

    commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Terraform-Azure1
+# Terraform Azure Infrastructure with Docker and Nginx
+
+This repository contains Terraform code to provision an Azure infrastructure, including a virtual machine with Docker installed, and deploy an Nginx container from a custom Docker image stored in Azure Container Registry.
+
+## Prerequisites
+
+Before you begin, ensure you have the following installed on your local machine:
+
+- [Terraform](https://www.terraform.io/downloads.html)
+- [Azure CLI](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
+- [Git](https://git-scm.com/downloads)
+
+## Setup Instructions
+
+### Step 1: Clone the Repository
+
+Clone this repository to your local machine using the following command:
+
+```bash
+git clone https://github.com/your-username/your-repo-name.git
+cd your-repo-name
--- run_nginx.tf
+++ run_nginx.tf
@@ -8,13 +8,14 @@ resource "null_resource" "run_nginx" {
     host     = azurerm_public_ip.pip[count.index].ip_address
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo docker login ${azurerm_container_registry.acr.login_server} -u ${azurerm_container_registry.acr.admin_username} -p ${azurerm_container_registry.acr.admin_password}",
-      "sudo docker pull ${azurerm_container_registry.acr.login_server}/nginx:latest",
-      "sudo docker run -d -p 80:80 ${azurerm_container_registry.acr.login_server}/nginx:latest",
-    ]
-  }
+   provisioner "remote-exec" {
+  inline = [
+    "sudo docker login ${azurerm_container_registry.acr.login_server} -u ${azurerm_container_registry.acr.admin_username} -p ${azurerm_container_registry.acr.admin_password}",
+    "sudo docker pull ${azurerm_container_registry.acr.login_server}/nginx:latest",
+    "sudo docker run -d -p 80:80 ${azurerm_container_registry.acr.login_server}/nginx:latest",
+ 
+  ]
+}
 
   depends_on = [
     azurerm_linux_virtual_machine.vm,
